68e9c060e2ef523282e6748dbaed826b3595ca44
周政伟 <zhou_13098240556@163.com>
2020-11-28 00:09:28 +0800

    完善了 Board.java, 还有部分函数没有实现。

--- src/main/java/entity/Board.java
+++ src/main/java/entity/Board.java
@@ -4,8 +4,10 @@ import exception.NonUniqueGizmoNameException;
 import exception.XMLLoadException;
 import util.BoardBuilderXML;
 import util.Display;
+import util.JavaUtilTimerClock;
 import util.KeyboardInput;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
@@ -28,11 +30,12 @@ import java.util.*;
  */
 public class Board implements Iterable<Gizmo>{
     public static final String rcsId = "202011271247";
-    private Display display;
-    private Vector<Integer> dimensions;
-    private final Vector<Float> friction;
-    private final double gravity;
-    private KeyboardInput input;
+    private Display displayMode;
+    private Vector<Integer> maxDims;
+    private Vector<Float> friction;
+    private double gravity;
+    private KeyboardInput inputMode;
+    private JavaUtilTimerClock boardClock;
 
     private List<Gizmo> boardGizmos;
 
@@ -40,27 +43,27 @@ public class Board implements Iterable<Gizmo>{
      * Creates a board by passing in all existing member variables.
      * * If friction or dimensions do not meet their specified capacities default values of
      * *  (0.025/s,0.025/L) and (20,20) respectively are used.
-     * @param dimensions: the x, y max coordinates
+     * @param maxDims: the x, y max coordinates
      * @param gravity: coefficient of gravity
      * @param friction: coefficient of friction
-     * @param display: which display to be used (ascii, gl, etc)
-     * @param input: KeyboardInput
+     * @param displayMode: which display to be used (ascii, gl, etc)
+     * @param inputMode: KeyboardInput
      * @require: xMax, yMax, grav, friction >= 0, dimensions.capacity = 2 friction.capacity = 2.
      * @effects: Creates a new Board
      * @modifies: All member variables
      */
-    public Board(Vector<Integer> dimensions, double gravity,
-                 Vector<Float> friction, Display display, KeyboardInput input){
+    public Board(Vector<Integer> maxDims, double gravity,
+                 Vector<Float> friction, Display displayMode, KeyboardInput inputMode){
         assert (gravity > 0.0);
-        assert (display != null && input != null);
+        assert (displayMode != null && inputMode != null);
 
-        if(dimensions.capacity() == 2) {
-            assert (dimensions.get(0) > 0 && dimensions.get(1) > 0);
-            this.dimensions = dimensions;
+        if(maxDims.capacity() == 2) {
+            assert (maxDims.get(0) > 0 && maxDims.get(1) > 0);
+            this.maxDims = maxDims;
         } else {
-            this.dimensions = new Vector<>(2, 0);
-            this.dimensions.add(20);
-            this.dimensions.add(20);
+            this.maxDims = new Vector<>(2, 0);
+            this.maxDims.add(20);
+            this.maxDims.add(20);
         }
 
         this.gravity = gravity;
@@ -74,8 +77,8 @@ public class Board implements Iterable<Gizmo>{
             this.friction.add((float) 0.025);
         }
 
-        this.display = display;
-        this.input = input;
+        this.displayMode = displayMode;
+        this.inputMode = inputMode;
         this.boardGizmos = new LinkedList<>();
     }
 
@@ -106,8 +109,6 @@ public class Board implements Iterable<Gizmo>{
 
     /**
      * Thrown to indicate an error when loading a board XML file.
-     *
-     * TODO: More detailed specifications.
      * @param filename: a XML file's path.
      * @param boardInput: KeyboardInput
      * @return: a Board created by XML file and boardInput.
@@ -132,8 +133,8 @@ public class Board implements Iterable<Gizmo>{
      * @throws NonUniqueGizmoNameException: if someone tries to add a gizmo that does not have a unique name.
      */
     public void addGizmo(Gizmo gizmo) throws NonUniqueGizmoNameException {
-        assert (gizmo.getPosition().get(0) < dimensions.get(0));
-        assert (gizmo.getPosition().get(1) < dimensions.get(1));
+        assert (gizmo.getPosition().get(0) < maxDims.get(0));
+        assert (gizmo.getPosition().get(1) < maxDims.get(1));
 
         if(isNameUnique(gizmo.getName()))
             boardGizmos.add(gizmo);
@@ -161,6 +162,138 @@ public class Board implements Iterable<Gizmo>{
         return true;
     }
 
+    /**
+     * @param gizmo: the gizmo to be checked out
+     * @return: the removed gizmo, if the gizmo is not in the list returns null
+     * @require: gizmo is in boardGizmos
+     * @effects: removes gizmo from boardGizmos, if it exists, and returns it to the caller.
+     * @modifies: boardGizmos.
+     */
+    public Gizmo checkoutGizmo(Gizmo gizmo){
+        if(removeGizmo(gizmo))
+            return gizmo;
+        return null;
+    }
+
+    /**
+     * Saves the Board to an xml file at pathName
+     * @param pathName: a xml file in an existing folder
+     * @effects: Creates a .xml file representing this that can be loaded later.
+     * Note that information on the board's dimension, the input type, clock and other state specific information
+     *  is not included in the xml file.
+     */
+    public void saveToFile(File pathName){
+        new BoardBuilderXML().buildFileFromBoard(this, pathName);
+    }
+
+    /**
+     *
+     * @param gizmo: the gizmo to be removed
+     * @return: a boolean indicating true if the gizmo existed in the board and was removed and false otherwise
+     * @effects: removes the first `Gizmo` object in boardGizmos that .equals gizmo
+     * @modifies: boardGizmos
+     */
+    public boolean removeGizmo(Gizmo gizmo){
+        return boardGizmos.remove(gizmo);
+    }
+
+    /**
+     * Step will only run if it is not currently being called
+     * AND the board is 'ready' as determined by the method isBoardReady().
+     *
+     * Calls to step will be not attempt to step the components of the board
+     *  if the board is not ready and the method call will return false.
+     *
+     * The method will still wait for a step method to finish if it is in progress even if the board isn't ready.
+     *
+     * If step is currently being run, step will wait for the previous call to finish and then run.
+     *
+     * As such, a good program will make sure that it isn't making multiple simultaneous calls to step
+     * and will be sure that the board is ready before calling step in order to avoid extraneous steps.
+     *
+     * @param timeSinceLastStep: int
+     * @return: Whether or not the board was ready and steps were called on board elements.
+     * @throws InterruptedException:  if the current thread is interrupted
+     * @effects: calls step on all board elements, effectively stepping the board.
+     * @TODO: step method.
+     */
+    public boolean step(int timeSinceLastStep) throws InterruptedException{
+        return true;
+    }
+
+    /**
+     * Checks to see if the board is fully initialized
+     * @return: true if xyMaxDims are all > 0, inputMode != null, displayMode != null, boardClock != null
+     */
+    public boolean isBoardReady(){
+        return maxDims.get(0) > 0 && maxDims.get(1) > 0 && inputMode != null && displayMode != null && boardClock != null;
+    }
+
+    /**
+     * @return: true if isBoardReady() is true and all gizmos are inside of the maximum dimensions of the board.
+     * @TODO: all gizmos are inside of the maximum dimensions of the board.
+     */
+    public boolean checkGizmos(){
+        return isBoardReady();
+    }
+
+    public Display getDisplayMode(){
+        return this.displayMode;
+    }
+
+    public Board setDisplayMode(Display displayMode){
+        this.displayMode = displayMode;
+        return this;
+    }
+
+    public KeyboardInput getInputMode(){
+        return this.inputMode;
+    }
+
+    public Board setInputMode(KeyboardInput inputMode){
+        this.inputMode = inputMode;
+        return this;
+    }
+
+    public Vector<Integer> getMaxDims(){
+        return this.maxDims;
+    }
+
+    public Board setMaxDims(Vector<Integer> maxDims){
+        this.maxDims = maxDims;
+        return this;
+    }
+
+    public Vector<Float> getFriction(){
+        return this.friction;
+    }
+
+    public Board setFriction(Vector<Float> friction){
+        this.friction = friction;
+        return this;
+    }
+
+    public double getGravity(){
+        return this.gravity;
+    }
+
+    public Board setGravity(double gravity){
+        this.gravity = gravity;
+        return this;
+    }
+
+    public Set<String> getNames(){
+        Set<String> namesSet = new HashSet<>();
+        for (Gizmo gizmo : boardGizmos) {
+            namesSet.add(gizmo.getName());
+        }
+        return namesSet;
+    }
+
+    /**
+     * Returns an iterator on an unmodifiable boardGizmos.
+     * @return: the iterator of the boardGizmos list
+     */
     @Override
     public Iterator<Gizmo> iterator() {
         return boardGizmos.iterator();
